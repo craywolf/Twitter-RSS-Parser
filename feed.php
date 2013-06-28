40b6de201632b11c4447dac3f6fb119e9a9ac526
craywolf <craywolf@example.com>
2013-06-28 13:51:14 -0400

    Add rel=noreferrer and target=_blank to links

--- feed.php
+++ feed.php
@@ -6,6 +6,8 @@ if ($search){
 	$td = $twitter_data;
 }
 $arrLen = count($td);
+$linkOpts = 'rel="noreferrer" target="_blank"';
+
 for ($i=0; $i<$arrLen; $i++) {
 	print(PHP_EOL. '	<entry>'. PHP_EOL);
 		print('		<id>tag:twitter.com,' . date("Y-m-d", strtotime($td[$i]['created_at'])) . ':/' . $td[$i]['user']['screen_name'] . '/statuses/' . $td[$i]['id_str'] . '</id>'. PHP_EOL);
@@ -20,7 +22,7 @@ for ($i=0; $i<$arrLen; $i++) {
 			$url = $td[$i]['entities']['urls'][$j]['url'];
 			$expanded_url = $td[$i]['entities']['urls'][$j]['expanded_url'];
 			$display_url = $td[$i]['entities']['urls'][$j]['display_url'];
-			$linkstr = '<a href="'.$expanded_url.'">'.$display_url.'</a>';
+			$linkstr = '<a href="'.$expanded_url.'" '.$linkOpts.'>'.$display_url.'</a>';
 			
 			$summaryContent = str_replace($url, $display_url, $summaryContent);
 			$feedContent = str_replace($url, $linkstr, $feedContent);
@@ -29,7 +31,7 @@ for ($i=0; $i<$arrLen; $i++) {
 		// Now loop through and linkify mentions
 		for ($j = 0; $j < count($td[$i]['entities']['user_mentions']); $j++) {
 			$screen_name = $td[$i]['entities']['user_mentions'][$j]['screen_name'];
-			$linkstr = '<a href="http://twitter.com/'.$screen_name.'">@'.$screen_name.'</a>';
+			$linkstr = '<a href="http://twitter.com/'.$screen_name.'" '.$linkOpts.'>@'.$screen_name.'</a>';
 			
 			$feedContent = str_replace('@'.$screen_name, $linkstr, $feedContent);
 		}
@@ -37,7 +39,7 @@ for ($i=0; $i<$arrLen; $i++) {
 		// And linkify hashtags
 		for ($j = 0; $j < count($td[$i]['entities']['hashtags']); $j++) {
 			$hash_text = $td[$i]['entities']['hashtags'][$j]['text'];
-			$linkstr = '<a href="http://twitter.com/search?q=%23'.$hash_text.'&src=hash">#'.$hash_text.'</a>';
+			$linkstr = '<a href="http://twitter.com/search?q=%23'.$hash_text.'&src=hash" '.$linkOpts.'>#'.$hash_text.'</a>';
 			
 			$feedContent = str_replace('#'.$hash_text, $linkstr, $feedContent);
 		}
@@ -47,11 +49,11 @@ for ($i=0; $i<$arrLen; $i++) {
 		//       Be sure to limit recursion if you do this.
 		if ($td[$i]['in_reply_to_status_id']) {
 			$parent_sn = $td[$i]['in_reply_to_screen_name'];
-			$parent_link = '<a href="http://twitter.com/'.$parent_sn.'/statuses/'.$td[$i]['in_reply_to_status_id_str'].'">@'.$parent_sn.'</a>';
+			$parent_link = '<a href="http://twitter.com/'.$parent_sn.'/statuses/'.$td[$i]['in_reply_to_status_id_str'].'" '.$linkOpts.'>@'.$parent_sn.'</a>';
 			
 			$feedContent = '<p><em>In reply to '.$parent_link.':</em></p>'.PHP_EOL.$feedContent;
 		}
-			
+		
 		
 		print('		<title>'.$td[$i]['user']['screen_name'].': '.htmlspecialchars($summaryContent).'</title>'. PHP_EOL);
 		print('		<summary type="html"><![CDATA['.$td[$i]['user']['screen_name'].': '.$summaryContent.']]></summary>'. PHP_EOL);
