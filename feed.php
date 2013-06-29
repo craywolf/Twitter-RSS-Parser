d72155dc9509d64c63363fac425f7ccd0af21adf
craywolf <craywolf@example.com>
2013-06-28 20:07:11 -0400

    Separate embedding vs linking photos

--- feed.php
+++ feed.php
@@ -74,15 +74,8 @@ for ($i=0; $i<$arrLen; $i++) {
 		if(isset($td[$i]['entities']['media'])) {
 			for ($j = 0; $j < count($td[$i]['entities']['media']); $j++) {
 				if ($td[$i]['entities']['media'][$j]['type'] == 'photo') {
-					$url = $td[$i]['entities']['media'][$j]['url'];
-					$display_url = $td[$i]['entities']['media'][$j]['display_url'];
 					$media_url = $td[$i]['entities']['media'][$j]['media_url'];
-					$expanded_url = $td[$i]['entities']['media'][$j]['expanded_url'];
 					
-					$media_link = '<a href="'.$expanded_url.'" '.$linkOpts.'>'.$display_url.'</a>';
-					
-					$summaryContent = str_replace($url, $display_url, $summaryContent);
-					$feedContent = str_replace($url, $media_link, $feedContent);
 					$feedContent = $feedContent . PHP_EOL . '<p><img src="'.$media_url.'" /></p>';
 				}
 			}
